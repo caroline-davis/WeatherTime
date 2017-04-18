4a6c1cba67ac67f9807b791bf2c3a0c3a5b06ae0
Caroline Davis <caroline_davis@live.com>
2017-04-18 17:39:24 +1000

    first commit

---
+++ RainyShinyCloudy/WeatherVC.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherVC.swift
+//  RainyShinyCloudy
+//
+//  Created by Caroline Davis on 18/04/2017.
+//  Copyright © 2017 Caroline Davis. All rights reserved.
+//
+
+import UIKit
+
+
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+    }
+
+
+    
+
+
+}
+
